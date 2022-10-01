5297ec2925277d0dafdbe52f368e12e6c928fcd9
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2022-10-01 11:56:48 -0400

    Update DiComponent.kt

--- app/src/main/java/com/example/mvvmretrofitdaggerkt/di/DiComponent.kt
+++ app/src/main/java/com/example/mvvmretrofitdaggerkt/di/DiComponent.kt
@@ -1,17 +1,18 @@
 package com.example.mvvmretrofitdaggerkt.di
 
-import com.example.mvvmretrofitdaggerkt.model.BookApi
 import com.example.mvvmretrofitdaggerkt.model.BookRepository
+import com.example.mvvmretrofitdaggerkt.view.BaseActivity
 import com.example.mvvmretrofitdaggerkt.viewmodel.BookViewModel
+import com.example.mvvmretrofitdaggerkt.viewmodel.ViewModelFactory
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [DiModule::class])
 interface DiComponent {
-    fun provideBookApi(): BookApi
     fun provideBookRepository(): BookRepository
+    fun provideViewModelFactory(): ViewModelFactory
 
-    fun inject(repository: BookRepository)
     fun inject(bookViewModel: BookViewModel)
+    fun inject(baseActivity: BaseActivity)
 }
