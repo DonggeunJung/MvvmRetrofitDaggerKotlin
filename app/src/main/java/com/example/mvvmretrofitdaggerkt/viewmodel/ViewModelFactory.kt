c263e1a933d01226da6460f305eebffceebd0538
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2022-10-01 11:59:16 -0400

    Update ViewModelFactory.kt

--- app/src/main/java/com/example/mvvmretrofitdaggerkt/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/example/mvvmretrofitdaggerkt/viewmodel/ViewModelFactory.kt
@@ -2,15 +2,16 @@ package com.example.mvvmretrofitdaggerkt.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.mvvmretrofitdaggerkt.model.BookRepository
 import javax.inject.Singleton
 
 @Singleton
-class ViewModelFactory: ViewModelProvider.Factory {
+class ViewModelFactory(var bookRepository: BookRepository): ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass == BookViewModel::class.java) {
-            return BookViewModel() as T
+            return BookViewModel(bookRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: \${modelClass.name}")
     }
