9441b0f39618ee63227bb4b25c0bd33c05ca5e52
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2022-10-01 17:57:02 -0400

    Update ViewModelFactory.kt

--- app/src/main/java/com/example/mvvmretrofitdaggerkt/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/example/mvvmretrofitdaggerkt/viewmodel/ViewModelFactory.kt
@@ -3,15 +3,21 @@ package com.example.mvvmretrofitdaggerkt.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmretrofitdaggerkt.model.BookRepository
+import javax.inject.Inject
+import javax.inject.Provider
 import javax.inject.Singleton
 
 @Singleton
-class ViewModelFactory(var bookRepository: BookRepository): ViewModelProvider.Factory {
+class ViewModelFactory @Inject constructor(bookViewModelProdiver: Provider<BookViewModel>): ViewModelProvider.Factory {
+
+    private val providers = mapOf<Class<*>, Provider<out ViewModel>>(
+        BookViewModel::class.java to bookViewModelProdiver
+    )
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass == BookViewModel::class.java) {
-            return BookViewModel(bookRepository) as T
+        if(providers.contains(modelClass)) {
+            return providers[modelClass]!!.get() as T
         }
         throw IllegalArgumentException("Unknown ViewModel class: \${modelClass.name}")
     }
