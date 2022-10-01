794b5b153f863a4ba0bfad2a3efe6d2e2ae38e70
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2022-10-01 11:58:48 -0400

    Update BookViewModel.kt

--- app/src/main/java/com/example/mvvmretrofitdaggerkt/viewmodel/BookViewModel.kt
+++ app/src/main/java/com/example/mvvmretrofitdaggerkt/viewmodel/BookViewModel.kt
@@ -3,22 +3,14 @@ package com.example.mvvmretrofitdaggerkt.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.mvvmretrofitdaggerkt.di.DaggerDiComponent
 import com.example.mvvmretrofitdaggerkt.model.Book
 import com.example.mvvmretrofitdaggerkt.model.BookRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import javax.inject.Inject
 
-class BookViewModel : ViewModel() {
-    @Inject
-    lateinit var bookRepository: BookRepository
+class BookViewModel(var bookRepository: BookRepository) : ViewModel() {
     val booksLiveData = MutableLiveData<List<Book>>()
-
-    init {
-        DaggerDiComponent.builder().build().inject(this)
-    }
 
     // Request Card data list to Repository
     fun reqBooks() {
