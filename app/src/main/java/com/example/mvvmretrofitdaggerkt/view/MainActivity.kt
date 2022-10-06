0c6b8f65e087d5b74be20218f968cdf2e84e7317
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2022-10-06 10:14:32 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/mvvmretrofitdaggerkt/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmretrofitdaggerkt/view/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.mvvmretrofitdaggerkt.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmretrofitdaggerkt.R
+import com.example.mvvmretrofitdaggerkt.viewmodel.BookViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     private lateinit var rvAdapter: BookRvAdapter
+    private val viewModel : BookViewModel by viewModels { viewModelFactory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
