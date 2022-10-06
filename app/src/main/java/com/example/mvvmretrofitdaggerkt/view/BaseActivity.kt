7304882129e9318dde7d8493fb320a4f37ba42e5
DONGGEUN JUNG <40321421+DonggeunJung@users.noreply.github.com>
2022-10-06 10:14:07 -0400

    Update BaseActivity.kt

--- app/src/main/java/com/example/mvvmretrofitdaggerkt/view/BaseActivity.kt
+++ app/src/main/java/com/example/mvvmretrofitdaggerkt/view/BaseActivity.kt
@@ -1,14 +1,10 @@
 package com.example.mvvmretrofitdaggerkt.view
 
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mvvmretrofitdaggerkt.di.DaggerDiComponent
-import com.example.mvvmretrofitdaggerkt.viewmodel.BookViewModel
 
 open class BaseActivity : AppCompatActivity() {
 
-    protected val viewModel: BookViewModel by viewModels {
-        DaggerDiComponent.builder().build().viewModelFactory()
-    }
+    protected val viewModelFactory = DaggerDiComponent.builder().build().viewModelFactory()
 
 }
